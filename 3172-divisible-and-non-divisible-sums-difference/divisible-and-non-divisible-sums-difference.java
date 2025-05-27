cd6ddbed8f9d1cc685eb41a44c355e7e7ea7d03d
Varsha Bhaskar <104689740+varshabhaskar07@users.noreply.github.com>
2025-05-27 06:57:16 +0530

    Time: 1 ms (74.69%) | Memory: 40.9 MB (40.47%) - LeetSync

--- 3172-divisible-and-non-divisible-sums-difference/divisible-and-non-divisible-sums-difference.java
+++ 3172-divisible-and-non-divisible-sums-difference/divisible-and-non-divisible-sums-difference.java
@@ -1,22 +1,24 @@
 class Solution {
     public int differenceOfSums(int n, int m) {
-        List<Integer> nums1 = new ArrayList<>();
-        List<Integer> nums2 = new ArrayList<>();
+        // List<Integer> nums1 = new ArrayList<>();
+        // List<Integer> nums2 = new ArrayList<>();
+        int nums1 = 0;
+        int nums2 =0;
 
         for (int i = 1; i <= n; i++) {
             if (i % m == 0) {
-                nums1.add(i);
+                nums1 += i;
             } else {
-                nums2.add(i);
+                nums2 += i;
             }
         }
 
-        int numSum1 = 0;
-        int numSum2 = 0;
+        // int numSum1 = 0;
+        // int numSum2 = 0;
 
-        for (int num : nums1) numSum1 += num;
-        for (int num : nums2) numSum2 += num;
+        // for (int num : nums1) numSum1 += num;
+        // for (int num : nums2) numSum2 += num;
 
-        return numSum2 - numSum1;
+        return nums2 - nums1;
     }
 }
